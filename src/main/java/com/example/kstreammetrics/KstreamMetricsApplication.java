ee68a78f257322b517f6516f0f707ef36ebfda4c
Soby Chacko <schacko@pivotal.io>
2021-08-20 16:37:17 -0400

    Initial commit

---
+++ src/main/java/com/example/kstreammetrics/KstreamMetricsApplication.java
@@ -0,0 +1,91 @@
+package com.example.kstreammetrics;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.Joined;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class KstreamMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KstreamMetricsApplication.class, args);
+	}
+
+	@Bean
+	public Function<KStream<Object, String>, KStream<String, WordCount>> process() {
+
+		return input -> input
+				.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+				.map((key, value) -> new KeyValue<>(value, value))
+				.groupByKey(Grouped.with(Serdes.String(), Serdes.String()))
+				.windowedBy(TimeWindows.of(5000))
+				.count(Materialized.as("foo-WordCounts"))
+				.toStream()
+				.map((key, value) -> new KeyValue<>(key.key(), new WordCount(key.key(), value,
+						new Date(key.window().start()), new Date(key.window().end()))));
+	}
+
+	static class WordCount {
+
+		private String word;
+
+		private long count;
+
+		private Date start;
+
+		private Date end;
+
+		WordCount(String word, long count, Date start, Date end) {
+			this.word = word;
+			this.count = count;
+			this.start = start;
+			this.end = end;
+		}
+
+		public String getWord() {
+			return word;
+		}
+
+		public void setWord(String word) {
+			this.word = word;
+		}
+
+		public long getCount() {
+			return count;
+		}
+
+		public void setCount(long count) {
+			this.count = count;
+		}
+
+		public Date getStart() {
+			return start;
+		}
+
+		public void setStart(Date start) {
+			this.start = start;
+		}
+
+		public Date getEnd() {
+			return end;
+		}
+
+		public void setEnd(Date end) {
+			this.end = end;
+		}
+	}
+}
---
+++ src/test/java/com/example/kstreammetrics/KstreamMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kstreammetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KstreamMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
